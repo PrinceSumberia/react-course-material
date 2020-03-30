a5c0598bc5d4bf9b2be7e6c063c31db955283bf4
Prince Sumberia <broughtmeup@gmail.com>
2020-03-30 23:26:08 +0530

    Add Sounds

--- pokedex/src/Pokecard.js
+++ pokedex/src/Pokecard.js
@@ -2,14 +2,26 @@ import React, { Component } from "react";
 import "./Pokecard.css";
 
 export default class Pokecard extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick = () => {
+    console.log("Playing");
+    let audio;
+    if (this.props.name === "Pikachu") {
+      audio = new Audio("./audio.mp3");
+    } else if (this.props.name === "Squirtle") {
+      audio = new Audio("./Squirtle.mp3");
+    }
+    audio.play();
+  };
   render() {
     const { id, name, type, base_experience } = this.props;
-    // const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
     const updatedId = ("0000" + id).slice(-3);
-    console.log(updatedId);
     const img = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${updatedId}.png`;
     return (
-      <div className="Pokecard">
+      <div className="Pokecard" onClick={this.handleClick}>
         <h3>{name}</h3>
         <img src={img} alt={name} />
         <p>{type}</p>
