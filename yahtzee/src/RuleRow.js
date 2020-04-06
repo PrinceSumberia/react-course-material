2d902a43898a509086fb3eb16eeb09bf65f5983d
Prince Sumberia <broughtmeup@gmail.com>
2020-04-06 18:07:55 +0530

    Fix bugs

--- yahtzee/src/RuleRow.js
+++ yahtzee/src/RuleRow.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
-import './RuleRow.css'
+import React, { Component } from "react";
+import "./RuleRow.css";
 
 class RuleRow extends Component {
   render() {
+    const disabled = this.props.score !== undefined;
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+      <tr
+        onClick={disabled ? null : this.props.doScore}
+        className={
+          this.props.score
+            ? "RuleRow RuleRow-disabled"
+            : "RuleRow RuleRow-active"
+        }
+      >
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score}</td>
       </tr>
-    )
+    );
   }
 }
 
