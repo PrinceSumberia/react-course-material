7a46a1068444d2e680baab7efcf9945b4fb19825
Prince Sumberia <broughtmeup@gmail.com>
2020-04-18 13:12:20 +0530

    initial commit

--- context-app-demo/src/App.js
+++ context-app-demo/src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>The React App</h1>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
