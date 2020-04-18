06ea3597d48e373091d69e44e228be88d3d080ec
Prince Sumberia <broughtmeup@gmail.com>
2020-04-18 14:17:18 +0530

    Add ToogleTheme

--- context-app-demo/src/Navbar.js
+++ context-app-demo/src/Navbar.js
@@ -15,7 +15,7 @@ import { ThemeContext } from "./contexts/ThemeContext";
 class Navbar extends Component {
   static contextType = ThemeContext;
   render() {
-    const { isDarkMode } = this.context;
+    const { isDarkMode, toggleTheme } = this.context;
     const { classes } = this.props;
     return (
       <div className={classes.root}>
@@ -27,7 +27,7 @@ class Navbar extends Component {
             <Typography className={classes.title} variant="h6" color="inherit">
               The App
             </Typography>
-            <Switch />
+            <Switch onChange={toggleTheme} />
             <div className={classes.grow} />
             <div className={classes.search}>
               <div className={classes.searchIcon}>
--- context-app-demo/src/contexts/ThemeContext.js
+++ context-app-demo/src/contexts/ThemeContext.js
@@ -8,11 +8,18 @@ export class ThemeProvider extends Component {
     this.state = {
       isDarkMode: true,
     };
+    this.toggleTheme = this.toggleTheme.bind(this);
+  }
+
+  toggleTheme() {
+    this.setState({ isDarkMode: !this.state.isDarkMode });
   }
 
   render() {
     return (
-      <ThemeContext.Provider value={{ ...this.state }}>
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
         {this.props.children}
       </ThemeContext.Provider>
     );
